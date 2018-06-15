be07a05c884399079d70ea4f32d49c76a10e61f2
Xwilarg <xwilarg@yahoo.fr>
2018-06-16 00:28:44 +0200

    Add orc movements

---
+++ Assets/Scripts/Ice/FallDetector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FallDetector : MonoBehaviour {
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            collision.GetComponent<Rigidbody2D>().AddTorque(float.MaxValue);
+    }
+}
