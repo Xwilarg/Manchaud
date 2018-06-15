01de7bfb6ef588268f58f728de0955ee4ea4d200
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-06-16 00:38:55 +0200

    Chracter move left and right

--- Assets/Scripts/House/Character.cs
+++ Assets/Scripts/House/Character.cs
@@ -1,29 +1,43 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class Character : MonoBehaviour {
 
     private Rigidbody2D rb;
     private bool stairs;
     [SerializeField]
-    private Collider2D room;
+    private Room room;
+    [SerializeField]
+    private float speed;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        room = other;
+        room = other.GetComponent<Room>();
     }
 
     // Use this for initialization
     private void Start () {
-        stairs = true;
+        stairs = room.HasStairs();
         rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	private void Update () {
         if (stairs)
-            rb.MovePosition(room.GetComponent<Room>().Down.GetComponent<Rigidbody2D>().position);
+        {
+            rb.MovePosition(room.GetDown().GetComponent<Rigidbody2D>().position);
+        }
         else
-            rb.velocity = new Vector2(5 * Time.deltaTime, 0); // x > 0 right || x < 0 left
-	}
+        {
+            if (room.GetLeft() != null)
+            {
+                rb.velocity = new Vector2(-speed * Time.deltaTime, 0); // x > 0 right || x < 0 left
+            }
+            else if (room.GetRight() != null)
+            {
+                rb.velocity = new Vector2(speed * Time.deltaTime, 0); // x > 0 right || x < 0 left
+            }
+        }
+    }
 }
--- Assets/Scripts/House/Room.cs
+++ Assets/Scripts/House/Room.cs
@@ -1,16 +1,40 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Room : MonoBehaviour {
     [SerializeField]
     private bool stairs;
-
-    public Room Up { get; }
-    public Room Down { get; }
-    public Room Left { get; }
-    public Room Right { get; }
+    [SerializeField]
+    private Room up;
+    [SerializeField]
+    private Room down;
+    [SerializeField]
+    private Room left;
+    [SerializeField]
+    private Room right;
 
     public bool HasStairs()
     {
         return stairs;
+    }
+
+    public Room GetUp()
+    {
+        return up;
+    }
+
+    public Room GetDown()
+    {
+        return down;
+    }
+
+    public Room GetLeft()
+    {
+        return left;
+    }
+
+    public Room GetRight()
+    {
+        return right;
     }
 }
