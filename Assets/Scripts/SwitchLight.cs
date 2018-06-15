8fb87346987b838fac0c30ae04858f673b582c42
Xwilarg <xwilarg@yahoo.fr>
2018-06-15 23:06:03 +0200

    Add light prefab

---
+++ Assets/Scripts/SwitchLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SwitchLight : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite spriteOn, spriteOff;
+    private bool isOn;
+    private SpriteRenderer currSprite;
+
+    private void Start()
+    {
+        currSprite = GetComponent<SpriteRenderer>();
+        isOn = (currSprite.sprite == spriteOn);
+    }
+
+    private void OnMouseDown()
+    {
+        isOn = !isOn;
+        currSprite.sprite = (isOn) ? (spriteOn) : (spriteOff);
+    }
+}
