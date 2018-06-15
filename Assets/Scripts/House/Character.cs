9484f710a4c29064b491746f0a6bb687cafdec13
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-06-16 01:28:14 +0200

    Character taking stairs

--- Assets/Scripts/House/Character.cs
+++ Assets/Scripts/House/Character.cs
@@ -2,42 +2,62 @@
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Character : MonoBehaviour {
 
     private Rigidbody2D rb;
     private bool stairs;
     [SerializeField]
-    private Room room;
+    private Room room, oldRoom;
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private Sprite spriteLeft, spriteRight;
+    private SpriteRenderer renderer;
+    private int direction;
+    private bool tookStairs;
+    private enum Directions { LEFT, RIGHT };
+    [SerializeField]
+    private Vector3 offset;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        room = other.GetComponent<Room>();
+        if (other.CompareTag("Room"))
+        {
+            oldRoom = room;
+            room = other.GetComponent<Room>();
+            if (tookStairs == false && room.HasStairs())
+            {
+                if (room.GetDown() != null && room.GetDown() != oldRoom)
+                    rb.MovePosition(room.GetDown().transform.position - offset);
+                else if (room.GetUp() != null && room.GetUp() != oldRoom)
+                    rb.MovePosition(room.GetDown().transform.position);
+                tookStairs = true;
+            }
+        }
     }
 
-    // Use this for initialization
     private void Start () {
         stairs = room.HasStairs();
         rb = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        direction = (int)Directions.RIGHT;
+        tookStairs = true;
 	}
 	
-	// Update is called once per frame
 	private void Update () {
-        if (stairs)
+            tookStairs = false;
+        if (direction != (int)Directions.LEFT && room.GetLeft() != null)
         {
-            rb.MovePosition(room.GetDown().GetComponent<Rigidbody2D>().position);
+            direction = (int)Directions.LEFT;
+            renderer.sprite = spriteLeft;
+            rb.velocity = new Vector2(-speed * Time.deltaTime, 0); // x > 0 right || x < 0 left
         }
-        else
+        else if (direction != (int)Directions.RIGHT && room.GetRight() != null)
         {
-            if (room.GetLeft() != null)
-            {
-                rb.velocity = new Vector2(-speed * Time.deltaTime, 0); // x > 0 right || x < 0 left
-            }
-            else if (room.GetRight() != null)
-            {
-                rb.velocity = new Vector2(speed * Time.deltaTime, 0); // x > 0 right || x < 0 left
-            }
+            direction = (int)Directions.RIGHT;
+            renderer.sprite = spriteRight;
+            rb.velocity = new Vector2(speed * Time.deltaTime, 0); // x > 0 right || x < 0 left
         }
     }
 }
