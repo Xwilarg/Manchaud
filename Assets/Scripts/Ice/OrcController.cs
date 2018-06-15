ad10b5f4d332fd1c6611479c28bb24b4a21bc3a8
Xwilarg <xwilarg@yahoo.fr>
2018-06-16 01:36:50 +0200

    Add orc attack

--- Assets/Scripts/Ice/OrcController.cs
+++ Assets/Scripts/Ice/OrcController.cs
@@ -3,20 +3,45 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class OrcController : MonoBehaviour {
 
+    [Header("Movements")]
     [SerializeField]
     private Node firstNode;
     [SerializeField]
     private float speed;
     [SerializeField]
     private float step;
+    [Header("Attack")]
+    [SerializeField]
+    private float minTimeJump;
+    [SerializeField]
+    private float maxTimeJump;
+    [SerializeField]
+    private float jumpTime;
+    [SerializeField]
+    private float jumpForce;
 
+    private float attackTimer;
+    private float? jumpTimer;
+    private float? killTimer;
+
+    private GameObject player;
     private Node nextNode;
     private Rigidbody2D rb;
 
+    private void SetAttackTimer()
+    {
+        attackTimer = Random.Range(minTimeJump, maxTimeJump);
+    }
+
     private void Start()
     {
+        Debug.Assert(minTimeJump < maxTimeJump, "min time must be < to max time");
         nextNode = firstNode.GetNextNode();
         rb = GetComponent<Rigidbody2D>();
+        SetAttackTimer();
+        jumpTimer = null;
+        killTimer = null;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private int GetXDirection()
@@ -39,11 +64,40 @@ public class OrcController : MonoBehaviour {
 
     private void Update()
     {
-        float xDir = GetXDirection();
-        float yDir = GetYDirection();
-        if (xDir == 0 && yDir == 0)
-            nextNode = nextNode.GetNextNode();
+        attackTimer -= Time.deltaTime;
+        if (attackTimer < 0f)
+        {
+            SetAttackTimer();
+            jumpTimer = 2f;
+        }
+        if (jumpTimer == null)
+        {
+            float xDir = GetXDirection();
+            float yDir = GetYDirection();
+            if (xDir == 0 && yDir == 0)
+                nextNode = nextNode.GetNextNode();
+            else
+                rb.velocity = new Vector2(xDir * speed * Time.deltaTime, yDir * speed * Time.deltaTime);
+        }
+        else if (killTimer == null)
+        {
+            rb.velocity = Vector2.zero;
+            jumpTimer -= Time.deltaTime;
+            if (jumpTimer < 0f)
+            {
+                Vector2 dir = (transform.position - player.transform.position).normalized;
+                rb.AddForce(-dir * jumpForce, ForceMode2D.Impulse);
+                killTimer = jumpTime;
+            }
+        }
         else
-            rb.velocity = new Vector2(xDir * speed * Time.deltaTime, yDir * speed * Time.deltaTime);
+        {
+            killTimer -= Time.deltaTime;
+            if (killTimer < 0f)
+            {
+                killTimer = null;
+                jumpTimer = null;
+            }
+        }
     }
 }
