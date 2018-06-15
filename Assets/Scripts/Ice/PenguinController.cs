9db9b94302f918265b40d6c08a085111143f70dc
Xwilarg <xwilarg@yahoo.fr>
2018-06-15 23:47:41 +0200

    Change Input settings & add ice objects to scene

---
+++ Assets/Scripts/Ice/PenguinController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PenguinController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
