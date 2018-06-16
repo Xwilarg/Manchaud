0579cccab99153a6ba88f1613279ebe8674aa262
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-06-16 11:55:01 +0200

    add entry door + changing door layer

--- Assets/Scripts/Ice/FallDetector.cs
+++ Assets/Scripts/Ice/FallDetector.cs
@@ -5,6 +5,8 @@ public class FallDetector : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
+        {
             collision.GetComponent<Rigidbody2D>().AddTorque(float.MaxValue);
+        }
     }
 }
