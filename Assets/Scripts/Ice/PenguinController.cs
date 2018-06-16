fd0abca0b69e818fba37c924773745ea2764f712
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-06-16 19:16:38 +0200

    all directions but top penguins naimatiosn

--- Assets/Scripts/Ice/PenguinController.cs
+++ Assets/Scripts/Ice/PenguinController.cs
@@ -16,7 +16,7 @@ public class PenguinController : MonoBehaviour
     private GameObject gameOverPanel;
     private bool isAlive;
     [SerializeField]
-    private RuntimeAnimatorController controllerLeft, controllerRight;
+    private RuntimeAnimatorController controllerRight, controllerBotRight, controllerBot, controllerBotLeft, controllerLeft, controllerTopLeft, controllerTop, controllerTopRight;
     private Animator animator;
 
     private AudioSource source;
@@ -48,10 +48,22 @@ public class PenguinController : MonoBehaviour
             source.Play();
             walkTimer = source.clip.length + 0.2f;
         }
-        if (hor > 0) // Right
+        if (hor > 0.5f && (ver > -0.5f && ver < 0.5f)) // Right
             animator.runtimeAnimatorController = controllerRight;
-        else
+        else if (hor > 0.5f && ver < -0.5f) // Bottom-Right
+            animator.runtimeAnimatorController = controllerBotRight;
+        else if ((hor < 0.5f && hor > -0.5f) && ver < -0.5f)
+            animator.runtimeAnimatorController = controllerBot;
+        else if (hor < -0.5f && ver < -0.5f)
+            animator.runtimeAnimatorController = controllerBotLeft;
+        else if (hor < -0.5f && (ver > -0.5f && ver < 0.5f)) // Left
             animator.runtimeAnimatorController = controllerLeft;
+        else if (hor < -0.5f && ver > 0.5f)
+            animator.runtimeAnimatorController = controllerTopLeft;
+        else if ((hor < -0.5f && hor > 0.5f) && ver > 0.5f)
+            animator.runtimeAnimatorController = controllerTop;
+        else if (hor > 0.5f && ver > 0.5f)
+            animator.runtimeAnimatorController = controllerTopRight;
         Slide();
     }
 
