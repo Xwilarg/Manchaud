440d59565851ff58177456502e2743019b716c6a
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-06-16 03:16:41 +0200

    Play audio clip

--- Assets/Scripts/House/SwitchLight.cs
+++ Assets/Scripts/House/SwitchLight.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(AudioSource))]
 public class SwitchLight : MonoBehaviour
 {
     [SerializeField]
     private Sprite spriteOn, spriteOff;
     private bool isOn;
     private SpriteRenderer currSprite;
+    [SerializeField]
+    private Type type;
+    enum Type { FRIDGE, LIGHT, RADIATOR };
+    private AudioSource audioSrc;
+    [SerializeField]
+    private AudioClip clipOn, clipOff;
 
     private void Start()
     {
         currSprite = GetComponent<SpriteRenderer>();
         isOn = (currSprite.sprite == spriteOn);
+        audioSrc = GetComponent<AudioSource>();
     }
 
     private void OnMouseDown()
@@ -26,6 +34,7 @@ public class SwitchLight : MonoBehaviour
         {
             currSprite.sprite = spriteOn;
             isOn = true;
+            audioSrc.PlayOneShot(clipOn);
         }
     }
 }
