d959c885aaaaf0992db6c9fac1882e16a5628513
Xwilarg <xwilarg@yahoo.fr>
2018-06-16 16:41:53 +0200

    Narvel now can kill player

---
+++ Assets/Scripts/Ice/KillCollider.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class KillCollider : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            collision.GetComponent<PenguinController>().SetAlive(false);
+    }
+}
--- Assets/Scripts/Ice/OrcController.cs
+++ Assets/Scripts/Ice/OrcController.cs
@@ -118,15 +118,6 @@ public class OrcController : MonoBehaviour
         return (-1);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            collision.GetComponent<Rigidbody2D>().AddTorque(float.MaxValue);
-            collision.GetComponent<PenguinController>().SetAlive(false);
-        }
-    }
-
     private void PrepareAttack()
     {
         SetAttackTimer();
