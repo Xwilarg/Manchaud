227cdf714537eb1dad999b7db95a4e1959547356
Xwilarg <xwilarg@yahoo.fr>
2018-06-16 14:05:30 +0200

    Readd while loop in MusicManager

--- Assets/Scripts/Manager/MusicManager.cs
+++ Assets/Scripts/Manager/MusicManager.cs
@@ -74,7 +74,7 @@ public class MusicManager : MonoBehaviour
     private void FadeOut(float duration, AudioSource audioOut)
     {
         // Linearly Fade Out audio
-       // while (audioOut.volume > 0.1)
+        while (audioOut.volume > 0.1)
         {
             audioOut.volume = Mathf.Lerp(audioOut.volume, 0.0f, duration * Time.deltaTime);
         }
@@ -87,9 +87,9 @@ public class MusicManager : MonoBehaviour
     {
         audioIn.Play();
         // Linearly Fade In audio
-       // while (audioIn.volume < 0.9)
+        while (audioIn.volume < 0.9)
         {
-            audioIn.volume = Mathf.Lerp(audioIn.volume, 0.0f, duration * Time.deltaTime);
+            audioIn.volume = Mathf.Lerp(audioIn.volume, 1.0f, duration * Time.deltaTime);
         }
         // Set volume to 1 to finish the FadeIn
         audioIn.volume = 1.0f;
