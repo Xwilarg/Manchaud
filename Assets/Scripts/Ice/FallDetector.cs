c492f9bf089202be2d1ac9d1849c7a12511f9506
Xwilarg <xwilarg@yahoo.fr>
2018-06-16 18:59:21 +0200

    Penguin is no longer spinning when falling out of the ice

--- Assets/Scripts/Ice/FallDetector.cs
+++ Assets/Scripts/Ice/FallDetector.cs
@@ -5,9 +5,6 @@ public class FallDetector : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        {
-            collision.GetComponent<Rigidbody2D>().AddTorque(float.MaxValue);
             collision.GetComponent<PenguinController>().SetAlive(false);
-        }
     }
 }
