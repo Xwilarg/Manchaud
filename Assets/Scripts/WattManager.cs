32d4c72e4e2bb2ee79f3653361621693a83028e3
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-06-16 05:54:06 +0200

    Revert "Merge branch 'master' of https://github.com/Xwilarg/Manchaud"
    This reverts commit 2b770d2a30fb12fa0017842892a582a8839eea95, reversing
    changes made to a54f6b62a01c08b167daadd56b5d44afad128861.

--- Assets/Scripts/WattManager.cs
+++ Assets/Scripts/WattManager.cs
@@ -6,8 +6,6 @@ public class WattManager : MonoBehaviour
 {
     [SerializeField]
     private RectTransform cursor;
-    [SerializeField]
-    private Transform ice;
     private List<SwitchLight> allObjs;
 
     private void Start()
@@ -24,12 +22,8 @@ public class WattManager : MonoBehaviour
     private void Update()
     {
         float watt = allObjs.Sum(x => x.GetConso()) * 500f / 0.5f - 250f;
-        float cursorWatt = (watt > 250f) ? (250f) : (watt);
-        cursor.localPosition = new Vector2(0f, cursorWatt);
-        if (watt < 90)
-            watt = 0;
-        watt = watt * 0.1f / 250f * Time.deltaTime;
-        if (ice.localScale.x - watt > 0.1f)
-            ice.localScale = new Vector2(ice.localScale.x - watt, ice.localScale.y - watt);
+        if (watt > 250f)
+            watt = 250f;
+        cursor.localPosition = new Vector2(0f, watt);
     }
 }
