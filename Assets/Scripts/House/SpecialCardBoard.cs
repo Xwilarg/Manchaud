a23ea3c9f370b1146ae51b1768d8ea1754ba9c16
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-06-16 21:35:28 +0200

    add not cardboard

---
+++ Assets/Scripts/House/SpecialCardBoard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(AudioSource))]
+public class SpecialCardBoard : MonoBehaviour {
+    private int hits;
+    [SerializeField]
+    private int breakLimit;
+    [SerializeField]
+    private Sprite breakSprite;
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private AudioClip breakClip;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        hits = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Hit");
+        ++hits;
+    }
+
+    // Update is called once per frame
+    public void Update () {
+        if (hits == breakLimit)
+            Break();
+	}
+
+    public void Break()
+    {
+        spriteRenderer.sprite = breakSprite;
+        audioSource.PlayOneShot(breakClip);
+        Debug.Log("It's not a cardboard");
+    }
+}
