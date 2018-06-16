1733e108df12957f947b352e53d8a14e89265883
Xwilarg <xwilarg@yahoo.fr>
2018-06-16 03:56:09 +0200

    Add button to reset scene

---
+++ Assets/Scripts/DebugManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugManager : MonoBehaviour {
+
+	public void ResetScene()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
