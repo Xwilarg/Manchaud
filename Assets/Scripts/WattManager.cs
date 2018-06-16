b7e2c971527b15fce68d7ce983446583cd318061
Xwilarg <xwilarg@yahoo.fr>
2018-06-16 06:06:53 +0200

    Fix norm for CodeFactor

--- Assets/Scripts/WattManager.cs
+++ Assets/Scripts/WattManager.cs
@@ -21,7 +21,7 @@ public class WattManager : MonoBehaviour
 
     private void Update()
     {
-        float watt = allObjs.Sum(x => x.GetConso()) * 500f / 0.5f - 250f;
+        float watt = (allObjs.Sum(x => x.GetConso()) * 500f / 0.5f) - 250f;
         if (watt > 250f)
             watt = 250f;
         cursor.localPosition = new Vector2(0f, watt);
