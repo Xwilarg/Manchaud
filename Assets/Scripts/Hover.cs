a9cea27462dfcbb04bef15f4091f07c72e87e766
Xwilarg <xwilarg@yahoo.fr>
2018-06-17 04:29:19 +0200

    Add hover to counter

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    private bool goUp;
+    private const float offset = 0.15f;
+    private const float speed = 0.2f;
+    private float orrY;
+
+    private void Start()
+    {
+        goUp = true;
+        orrY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector2(0f, ((goUp) ? (speed) : (-speed)) * Time.deltaTime));
+        if (transform.position.y >= orrY + offset)
+            goUp = false;
+        else if (transform.position.y <= orrY - offset)
+            goUp = true;
+    }
+}
