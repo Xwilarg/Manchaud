d4b7eca28c1406d33ce63598d380053c159dff1a
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-06-17 02:08:44 +0200

    Shaking Camera

---
+++ Assets/Scripts/Camera/Shaking.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Shaking : MonoBehaviour
+{
+    private Vector3 tremblement;
+
+	private void Update ()
+    {
+        Vector3 target = new Vector3();
+        transform.position = tremblement;
+
+        if (tremblement.magnitude <= target.magnitude + 0.1 && tremblement.magnitude >= target.magnitude - 0.1)
+        {
+            target = Random.insideUnitCircle;
+            target.x -= 21.52f;
+        }
+
+        tremblement.x = Mathf.SmoothStep(tremblement.x, target.x, Time.deltaTime * 200);
+        tremblement.y = Mathf.SmoothStep(tremblement.y, target.y, Time.deltaTime * 200);
+        tremblement.z = Mathf.SmoothStep(tremblement.z, target.z, Time.deltaTime * 200);
+    }
+}
